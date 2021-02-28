2cab7162c5062b40b9a11041a6989b6dbf2317ce
aaron28 <aaron.lee28@outlook.com>
2021-03-01 00:32:40 +0700

    initial commit

--- EmployeeSQL/data analysis/query.sql
+++ EmployeeSQL/data analysis/query.sql
@@ -1,13 +1,55 @@
-SELECT * FROM actor LIMIT 100;
-SELECT * FROM address LIMIT 100;
-SELECT * FROM city LIMIT 100;
-SELECT * FROM country LIMIT 100;
-SELECT * FROM customer LIMIT 100;
-SELECT * FROM customer_list LIMIT 100;
-SELECT * FROM film LIMIT 100;
-SELECT * FROM film_actor LIMIT 100;
-SELECT * FROM inventory LIMIT 100;
-SELECT * FROM payment LIMIT 100;
-SELECT * FROM rental LIMIT 100;
-SELECT * FROM staff LIMIT 100;
-SELECT * FROM store LIMIT 100;
+-- Data Analysis #1 
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary  
+INTO TABLE employee_info
+FROM employees
+LEFT JOIN salaries 
+ON employees.emp_no = salaries.emp_no;
+
+-- Data Analysis #2 
+SELECT employees.first_name, employees.last_name, employees.hire_date
+INTO TABLE hire_date_filter
+FROM employees
+select * from hire_date_filter
+WHERE hire_date between '1986-01-01' and '1986-12-31';
+
+-- Data Analysis #3
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+INTO TABLE manager_department_info
+FROM departments
+LEFT JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+LEFT JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
+
+-- Data Analysis #4
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name 
+INTO TABLE employee_department
+FROM employees
+LEFT JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+LEFT JOIN departments 
+ON dept_emp.dept_no = departments.dept_no;
+
+-- Data Analysis #5
+SELECT employees.first_name, employees.last_name, employees.sex
+INTO TABLE hercules_b_filter
+FROM employees
+SELECT * FROM hercules_b_filter
+WHERE first_name like 'Hercules'
+AND last_name like 'B%';
+
+-- Data Analysis #6
+SELECT * FROM employee_department 
+WHERE dept_name like 'Sales';
+
+-- Data Analysis #7 
+SELECT * FROM employee_department 
+WHERE dept_name like 'Sales' 
+OR dept_name like 'Development';
+
+-- Data Analysis #8
+SELECT last_name as "last name",
+count(last_name)
+FROM employees
+GROUP BY last_name
+ORDER BY "count" DESC; 
--- EmployeeSQL/data engineering/query.sql
+++ EmployeeSQL/data engineering/query.sql
@@ -1,13 +1,49 @@
-SELECT * FROM actor LIMIT 100;
-SELECT * FROM address LIMIT 100;
-SELECT * FROM city LIMIT 100;
-SELECT * FROM country LIMIT 100;
-SELECT * FROM customer LIMIT 100;
-SELECT * FROM customer_list LIMIT 100;
-SELECT * FROM film LIMIT 100;
-SELECT * FROM film_actor LIMIT 100;
-SELECT * FROM inventory LIMIT 100;
-SELECT * FROM payment LIMIT 100;
-SELECT * FROM rental LIMIT 100;
-SELECT * FROM staff LIMIT 100;
-SELECT * FROM store LIMIT 100;
+-- Create new table
+CREATE TABLE departments (
+	dept_no VARCHAR PRIMARY KEY NOT NULL,
+	dept_name VARCHAR 
+);
+
+CREATE TABLE titles( 
+	title_id VARCHAR PRIMARY KEY NOT NULL,
+	title VARCHAR NOT NULL
+);
+
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY NOT NULL, 
+	emp_title_id VARCHAR NOT NULL, 
+	birth_date DATE NOT NULL, 
+	first_name VARCHAR NOT NULL, 
+	last_name VARCHAR NOT NULL, 
+	sex VARCHAR NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+);
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR NOT NULL, 
+	emp_no INT NOT NULL, 
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE salaries(
+	emp_no INT NOT NULL, 
+	salary INT NOT NULL, 
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+
+SELECT * FROM  departments;
+SELECT * FROM  titles;
+SELECT * FROM  employees;
+SELECT * FROM  dept_emp;
+SELECT * FROM  dept_manager;
+SELECT * FROM  salaries;
+
